4d3189e6ec499ad3ad750571c9e26ecdc39fa16a
Jack Zhou <xza194@sfu.ca>
2018-06-24 07:19:32 -0700

    Update L.lua
    Major overhaul on codebase

--- L.lua
+++ L.lua
@@ -1,48 +1,61 @@
 -- L
 -- jryzkns 2018
 
-L= {}
+-- jryzkns 2018
+-- TODO: stochastic formulations: rules table have functions that return strings instead of just a string
 
-function L:evolve(path, rules)
+L = {}
+
+L.path,L.size, L.angle, L.rules = "", 0, 0, {}
+
+L.drawtable = {}
+
+L.drawtable["F"] = function()
+        love.graphics.line(0,0,L.size,0)
+        love.graphics.translate(L.size,0)
+end
+
+L.extradrawchars = "SLRB"
+for i=1,string.len(L.extradrawchars) do L.drawtable[L.extradrawchars:sub(i,i)] = L.drawtable["F"] end
+
+L.drawtable["+"] = function() love.graphics.rotate(math.rad(L.angle)) end
+L.drawtable["-"] = function() love.graphics.rotate(-math.rad(L.angle)) end
+L.drawtable["["] = function() love.graphics.push() end
+L.drawtable["]"] = function() love.graphics.pop() end
+
+L.drawtable["f"] = function() -- a flower
+        local r,g,b = love.graphics.getColor()
+        love.graphics.setColor(223/255,102/255,255/255)
+        love.graphics.circle("fill",0,0,L.size*0.5)
+        love.graphics.setColor(r,g,b)
+end
+
+function L:init(path,size,angle,rules,iteration)
+        L.path,L.size,L.angle,L.rules,L.iteration = path,size,angle,rules,iteration
+        for i=1,iteration do path = L:evolve() end
+end
+
+function L:evolve()
         --takes in a string, a table of rules, and returns the evolved string
         local out = ""
-        -- stuff here
-        for i=1,string.len(path) do
-                local step = path:sub(i,i)
-                if rules[step] ~= nil then
-                        step = rules[step]
+        for i=1,string.len(L.path) do
+                local step = L.path:sub(i,i)
+                
+                -- if a rule exists for the current step:
+                if L.rules[step] ~= nil then
+                        step = L.rules[step]
                 end
-
+                
                 -- if no rule exists, insert step itself
                 out = out .. step 
         end
         return out
+ 
 end
 
-function L:render(path,size,angle,iteration,rules) -- angle here is taken in as degrees
-        
-        for i=1,iteration do
-                path = L:evolve(path,rules)
-        end
-        
+function L:draw()
         love.graphics.push()
-                for i=1,string.len(path) do
-                        step = path:sub(i,i)
-                        if ((step == "F") or (step == "L") or (step == "R") or (step == "B")) then
-                                love.graphics.line(0,0,size,0)
-                                love.graphics.translate(size,0)
-                        elseif ( (step == "f"))  then
-                                love.graphics.translate(size,0)
-                        elseif( (step == "+"))  then
-                                love.graphics.rotate(math.rad(angle))
-                        elseif( (step == "-"))  then
-                                love.graphics.rotate(-math.rad(angle))
-                        elseif( (step == "["))  then
-                                love.graphics.push()
-                        elseif( (step == "]"))  then
-                                love.graphics.pop()
-                        end
-                end
+                for i=1,string.len(L.path) do L.drawtable[L.path:sub(i,i)]() end
         love.graphics.pop()
 end
 
